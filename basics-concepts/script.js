aaa148410a2f69630ed4b6eda523a74a8f06183e
Filipi <filipisee@hotmail.com>
2021-12-11 14:13:48 -0300

    testing some basic concepts

---
+++ basics-concepts/script.js
@@ -0,0 +1,15 @@
+/* const ListRendering = {
+  data() {
+    return {
+      todos: [
+        { text: "Learn JavaScript" },
+        { text: "Learn Vue" },
+        { text: "Build something awesome" },
+      ],
+    };
+  },
+};
+
+Vue.createApp(ListRendering).mount("#list-rendering");
+ */
+
