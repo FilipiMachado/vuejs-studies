83b6ae9c0e3c944d0c5e60cd7715d2cf2af228f0
Filipi <filipisee@hotmail.com>
2021-12-13 21:14:41 -0300

    testing vuejs events

--- events/app.js
+++ events/app.js
@@ -2,8 +2,21 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
+      number: "",
     };
+  },
+  methods: {
+    setName(event) {
+      this.number = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
