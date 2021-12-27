e4bff5a56821995fead0c2ce6f43262538b88fbf
fiil <filipisee@hotmail.com>
2021-12-27 14:57:11 -0300

    small changes

--- events/app.js
+++ events/app.js
@@ -33,7 +33,19 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
+  },
+  methods: {
+    increaseCounter(num) {
+      this.counter += num;
+    },
+    decreaseCounter(num) {
+      this.counter -= num;
+    },
+    addName(event) {
+      this.name = event.target.value  
+    }
   },
 });
 
