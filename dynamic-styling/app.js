b25e7005473d4fe1755bba19ea890e71cc747d24
filipi <filipisee@hotmail.com>
2022-01-03 19:46:01 -0300

    small tests

--- dynamic-styling/app.js
+++ dynamic-styling/app.js
@@ -6,6 +6,17 @@ const app = Vue.createApp({
       boxCSelected: false,
     };
   },
+  computed: {
+    boxAClasses() {
+      return {active: this.boxASelected}
+    },
+    boxBClasses() {
+      return {active: this.boxBSelected}
+    },
+    boxCClasses() {
+      return {active: this.boxCSelected}
+    },
+  },
   methods: {
     boxSelected(box) {
       if (box === "A") {
@@ -17,7 +28,6 @@ const app = Vue.createApp({
       }
     },
   },
-  computed: {},
 });
 
 app.mount("#styling");
