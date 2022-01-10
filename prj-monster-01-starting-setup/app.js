85efed3d8ce0a383cab5398582d4916cb691072a
filipi <filipisee@hotmail.com>
2022-01-10 20:07:44 -0300

    tests

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -10,6 +10,7 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0
     };
   },
   computed: {
@@ -22,6 +23,8 @@ const app = Vue.createApp({
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
+      console.log(`This is the current round: ${this.currentRound}`)
       const attackValue = getRandomAttackValue(12, 5);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
@@ -31,11 +34,13 @@ const app = Vue.createApp({
       this.playerHealth -= attackValue;
     },
     specialAttackPlayer() {
+      this.currentRound++;
       const attackValue = getRandomAttackValue(25, 10);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
     },
     healPlayer() {
+      this.currentRound++;
       const healValue = getRandomHealValue(10, 5);
       this.playerHealth += healValue;
       this.attackPlayer();
