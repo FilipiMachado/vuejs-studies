eb16cf3b3468b9d080b04568e971efeb35963209
filipi <filipisee@hotmail.com>
2022-01-10 19:22:44 -0300

    small changes

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -1,6 +1,26 @@
+function getRandomAttackValue(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 const app = Vue.createApp({
-  data() {},
-  methods: {},
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
+  },
+  methods: {
+    attackMonster() {
+      const attackValue = getRandomAttackValue(12, 5);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomAttackValue(18, 8);
+      this.playerHealth -= attackValue;
+    },
+  },
+  computed: {},
 });
 
 app.mount("#game");
