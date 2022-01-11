4fc3c6691f6cdb47ebd89285c495a87b66175130
filipi <filipisee@hotmail.com>
2022-01-11 10:20:47 -0300

    adding components

--- cmp-intro-01-starting-setup/app.js
+++ cmp-intro-01-starting-setup/app.js
@@ -1,13 +1,46 @@
 const app = Vue.createApp({
-    data(){
+  data() {
+    return {
+      detailsAreVisible: false,
+      friends: [
+        {
+          id: "zueiro",
+          name: "Zueiro Lorenz",
+          phone: "40028922",
+          email: "zueiro@gmail.com",
+        },
+        {
+          id: "peregrino",
+          name: "Peregrino Rulez",
+          phone: "87022514",
+          email: "peregrino@gmail.com",
+        },
+        {
+          id: "cegonha",
+          name: "Cegonha Blindada",
+          phone: "75463271",
+          email: "cegonha@gmail.com",
+        },
+      ],
+    };
+  },
+  computed: {},
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
+
+app.component('friend-contact', {
+    data() {
         return {
-            friends: {
-                id: "zueiro",
-                name: "Zueiro Lorenz",
-                phone: "40028922"
-            }
+
         }
+    },
+    methods: {
+        
     }
 })
 
-app.mount("#app")
+app.mount("#app");
