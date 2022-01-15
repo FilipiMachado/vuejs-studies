a35fc72d027207c8b76ef03af97897bc925acb44
filipi <filipisee@hotmail.com>
2022-01-15 15:32:26 -0300

    small changes

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <h2>My Friends</h2>
+        <ul>
+            <li>{{ friends[0] }}</li>
+            <li>{{ friends[1] }}</li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "filipi",
+          name: "Filipi",
+        },
+        {
+          id: "zueiro",
+          name: "Zueiro",
+        },
+      ],
+    };
+  },
+};
+</script>
