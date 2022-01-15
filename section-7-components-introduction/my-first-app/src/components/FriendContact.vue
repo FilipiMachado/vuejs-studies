ebc278cf2db0e5afaadf6ada1e89c817cfe155a2
filipi <filipisee@hotmail.com>
2022-01-15 16:44:30 -0300

    tests

--- section-7-components-introduction/my-first-app/src/components/FriendContact.vue
+++ section-7-components-introduction/my-first-app/src/components/FriendContact.vue
@@ -1,12 +1,12 @@
 <template>
-  <li>
+  <div>
     <h2>{{ friend.name }}</h2>
     <button v-on:click="toggleDetails">Show Details</button>
     <ul v-if="detailsAreVisible">
       <li><strong>Phone:</strong> {{ friend.phone }}</li>
-      <li><strong>Email:</strong> {{ friend.email }}</li>
+      <li><strong>Email:</strong> {{ friend.name }}</li>
     </ul>
-  </li>
+  </div>
 </template>
 
 <script>
@@ -14,26 +14,24 @@ export default {
   data() {
     return {
       detailsAreVisible: false,
-      friend: [
-        {
-          id: "filipi",
-          name: "Filipi",
-          phone: "184151854",
-          email: "filipi@gmail.com",
-        },
-        /* {
+      friend: {
+        id: "filipi",
+        name: "Filipi",
+        phone: "184151854",
+        email: "filipi@gmail.com",
+      },
+      /* {
           id: "zueiro",
           name: "Zueiro",
           phone: "841554184",
           email: "zueiro@gmail.com"
         } */
-      ],
     };
   },
   methods: {
-      toggleDetails() {
-          this.detailsAreVisible = !this.detailsAreVisible;
-      }
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
   },
 };
 </script>
