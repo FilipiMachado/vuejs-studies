37da1aba5d5e0ce05b7d37c70aee0710089885f9
filipi <filipisee@hotmail.com>
2022-01-17 23:41:23 -0300

    tests

--- section-7-components-introduction/my-first-app/src/App.vue
+++ section-7-components-introduction/my-first-app/src/App.vue
@@ -1,40 +1,48 @@
 <template>
-  <div id="app">
-    <header><h1>My Friends</h1></header>
+  <section>
+    <header>
+      <h1>My Friends</h1>
+    </header>
     <ul>
-      <FriendContact name="Zuero" phone-number="123456789" email-address="zuiei@gmail.com"/>
-      <FriendContact name="Peregrino" phone-number="987654321" email-address="peregrino@gmail.com"/>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
     </ul>
-  </div>
+  </section>
 </template>
 
 <script>
-import FriendContact from "./components/FriendContact.vue";
-
 export default {
-  name: "App",
-  components: {
-    FriendContact,
-  },
   data() {
-    return {};
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0987 654421 21",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
   },
 };
 </script>
 
-<style scoped>
+<style>
 * {
   box-sizing: border-box;
 }
-
 html {
-  font-family: 'Jost', sans-serif;
+  font-family: "Jost", sans-serif;
 }
-
 body {
   margin: 0;
 }
-
 header {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 3rem auto;
@@ -46,13 +54,11 @@ header {
   width: 90%;
   max-width: 40rem;
 }
-
 #app ul {
   margin: 0;
   padding: 0;
   list-style: none;
 }
-
 #app li {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
@@ -62,14 +68,12 @@ header {
   width: 90%;
   max-width: 40rem;
 }
-
 #app h2 {
   font-size: 2rem;
   border-bottom: 4px solid #ccc;
   color: #58004d;
   margin: 0 0 1rem 0;
 }
-
 #app button {
   font: inherit;
   cursor: pointer;
@@ -79,12 +83,10 @@ header {
   padding: 0.05rem 1rem;
   box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
 }
-
 #app button:hover,
 #app button:active {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
 }
-
 </style>
