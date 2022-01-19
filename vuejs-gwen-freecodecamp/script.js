7570f494436b98ecb1061560fed32c6ef22a7dff
filipi <filipisee@hotmail.com>
2022-01-18 21:26:16 -0300

    tests

--- vuejs-gwen-freecodecamp/script.js
+++ vuejs-gwen-freecodecamp/script.js
@@ -17,13 +17,29 @@ const app = Vue.createApp({
 })
 app.component("login-form", {
     template: `
-        <div>
+        <form v-on:submit.prevent="handleSubmit">
+            <h1>{{ title }}</h1>
             <h3>Email: </h3>
-            <input type="email"/>
+            <input type="email" v-model="email"/>
             <h3>Password: </h3>
-            <input type="password"/>
-        </div>
-    `
+            <input type="password" v-model="password"/>
+            <button id="submit-button">Submit</button>
+        </form>
+    `,
+    data(){
+        return {
+            title: "Login Form",
+            email: "",
+            password: "",
+        }
+    },
+    methods: {
+        handleSubmit(){
+            console.log(this.email, this.password);
+            this.email = ""
+            this.password = ""
+        }
+    },
 })
 
 app.mount("#first-app")
