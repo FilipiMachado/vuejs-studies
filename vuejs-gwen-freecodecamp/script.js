82dd9fc309ceb12b494fc1eb3a0087a5e0193f46
filipi <filipisee@hotmail.com>
2022-01-18 21:03:09 -0300

    tests[

--- vuejs-gwen-freecodecamp/script.js
+++ vuejs-gwen-freecodecamp/script.js
@@ -5,7 +5,25 @@ const app = Vue.createApp({
             isVisible: false,
             isVisible2: false,
         }
+    },
+    methods: {
+        showBox(){
+            this.isVisible = !this.isVisible
+        },
+        greet(greeting){
+            console.log(greeting)
+        }
     }
+})
+app.component("login-form", {
+    template: `
+        <div>
+            <h3>Email: </h3>
+            <input type="email"/>
+            <h3>Password: </h3>
+            <input type="password"/>
+        </div>
+    `
 })
 
 app.mount("#first-app")
