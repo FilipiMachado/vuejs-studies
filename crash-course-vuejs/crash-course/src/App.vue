553b9a8f12f31816c1d7f884bb53308cd566f17a
filipi <filipisee@hotmail.com>
2022-01-26 10:43:00 -0300

    added tasks

--- crash-course-vuejs/crash-course/src/App.vue
+++ crash-course-vuejs/crash-course/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="container">
     <Header title="Task Tracker"/>
+    <Tasks :tasks="tasks"/>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue"
+import Tasks from "./components/Tasks.vue"
 
 export default {
   name: "App",
   components: {
-    Header
+    Header,
+    Tasks
   },
   data(){
     return {
---
+++ crash-course-vuejs/crash-course/src/components/Tasks.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-for="task in tasks" :key="task.id">
+    <h3>
+      {{ task.text }}
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tasks",
+  props: {
+      tasks: Array
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
