79cdab69e0d1f117645abae92631618a7d670aa8
filipi <filipisee@hotmail.com>
2022-01-27 09:48:24 -0300

    tests

---
+++ crash-course-vuejs/crash-course/src/components/Task.vue
@@ -0,0 +1,17 @@
+<template>
+    <h3>{{ text.task }}</h3>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    task: Object,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
