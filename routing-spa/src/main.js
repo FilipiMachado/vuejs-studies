053b2524a99a39abf433d21e62b0ef9420756c59
filipi <filipisee@hotmail.com>
2022-02-10 11:00:39 -0300

    configuring vue router

--- routing-spa/src/main.js
+++ routing-spa/src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/teams', component } // our-domain/teams ...
+  ]
+})
 
 const app = createApp(App)
 
