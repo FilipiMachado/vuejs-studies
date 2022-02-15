2ffc90a42a228d44f9f7fe1742312de763918c42
filipi <filipisee@hotmail.com>
2022-02-15 10:15:45 -0300

    added router setup

--- routing-spa/src/main.js
+++ routing-spa/src/main.js
@@ -2,14 +2,19 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamsList from "./components/teams/TeamsList.vue"
+import UsersList from "./components/users/UsersList.vue"
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/teams', component } // our-domain/teams ...
+    { path: '/teams', TeamsList }, // our-domain/teams ...
+    { path: '/users', UsersList }, // our-domain/users
   ]
 })
 
 const app = createApp(App)
+
+app.use(router)
 
 app.mount('#app');
