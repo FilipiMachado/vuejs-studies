bbb5138d8a491afbd2161cf84db44a897c0cb9a0
filipi <filipisee@hotmail.com>
2022-02-15 20:24:05 -0300

    tests

--- routing-spa/src/main.js
+++ routing-spa/src/main.js
@@ -4,14 +4,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import TeamsList from "./components/teams/TeamsList.vue"
 import UsersList from "./components/users/UsersList.vue"
+import TeamMembers from "./components/teams/TeamMembers.vue"
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/teams', component: TeamsList }, // our-domain/teams
     { path: '/users', component: UsersList }, // our-domain/users
-    { path: '/teams/:teamId' },
-    { path: '/teams/new' }
+    { path: '/teams/:teamId', component: TeamMembers},
   ]
 })
 
