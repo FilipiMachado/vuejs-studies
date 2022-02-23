ead52057e14b309652eec1b0e19efaff5437422a
filipi <filipisee@hotmail.com>
2022-02-23 20:59:56 -0300

    small changes

---
+++ vuex-crash-course-state-management/src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button :style="{color}" class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+      text: String,
+      color: String
+    },
+    data() {
+      return {}
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
--- vuex-crash-course-state-management/src/components/Header.vue
+++ vuex-crash-course-state-management/src/components/Header.vue
@@ -1,12 +1,24 @@
 <template>
   <header>
-    <h1>Task Tracker</h1>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green"/>
   </header>
 </template>
 
 <script>
+import Button from "./Button.vue"
+
 export default {
   name: "Header",
+  components: {
+    Button
+  },
+  props: {
+    title: {
+      type: String,
+      default: "Task Tracker"
+    }
+  },
   data() {
     return {}
   },
