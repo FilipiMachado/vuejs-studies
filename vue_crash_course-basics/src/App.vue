57f870617313c6384210424e49cbd55ea77b48ee
filipi <filipisee@hotmail.com>
2022-02-24 22:14:36 -0300

    small changes

--- vue_crash_course-basics/src/App.vue
+++ vue_crash_course-basics/src/App.vue
@@ -42,9 +42,22 @@ export default {
           : alert("Error deleting task")
       }
     },
-    toggleReminder(id) {
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id)
+      const updateTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+      const res = await fetch(`api/tasks/${id}`, {
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updateTask)
+      })
+
+      const data = await res.json()
+
       this.tasks = this.tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       );
     },
     async addNewTask(task) {
