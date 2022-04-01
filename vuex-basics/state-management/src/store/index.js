cb222e43fab84d0e33d35a5f6ddaecb4d1fb75cf
filipi <filipisee@hotmail.com>
2022-04-01 09:22:39 -0300

    testing items

--- vuex-basics/state-management/src/store/index.js
+++ vuex-basics/state-management/src/store/index.js
@@ -4,29 +4,9 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    user: {
-      first_name: "",
-      last_name: "",
-      password: "",
-      email: "",
-    },
-  },
+  state: {},
   getters: {},
-  mutations: {
-    saveFirstName(state, payload) {
-      state.user.first_name = payload;
-    },
-    saveLastName(state, payload) {
-      state.user.last_name = payload;
-    },
-    savePassword(state, payload) {
-      state.user.password = payload;
-    },
-    saveEmail(state, payload) {
-      state.user.email = payload;
-    },
-  },
+  mutations: {},
   actions: {},
   modules: {},
 });
--- vuex-basics/state-management/src/views/HomeView.vue
+++ vuex-basics/state-management/src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
     <label>Nome</label>
-    <p>{{ data.name }}</p>
-    <input v-model="myName" type="text" />
+    <p>jk</p>
+    <input type="text" />
     <label>Sobrenome</label>
-    <p>{{ $store.state.user.last_name }}</p>
-    <input v-model="myLastName" type="text" />
+    <p>klçklç</p>
+    <input type="text" />
     <label>Senha</label>
-    <p>{{ $store.state.user.password }}</p>
-    <input v-model="myPassword" type="password" />
+    <p>asdasd</p>
+    <input type="password" />
     <label>Email</label>
-    <p>{{ $store.state.user.email }}</p>
-    <input v-model="myEmail" type="email" />
-    <button @click="saveUser">Salvar</button>
+    <p>iuuiiuo</p>
+    <input type="email" />
+    <button>Salvar</button>
   </div>
 </template>
 
@@ -21,34 +21,12 @@ export default {
   name: "HomeView",
   components: {},
   data() {
-    return {
-      myName: undefined,
-      myLastName: undefined,
-      myPassword: undefined,
-      myEmail: undefined,
-      data: {
-        name: "",
-        lastName: "",
-        password: "",
-        email: "",
-      },
-    };
+    return {};
   },
   methods: {
     saveUser() {
       // Mutation
       // this.$store.commit();
-      this.$store.commit("saveFirstName", this.myName);
-      this.$store.commit("saveLastName", this.myLastName);
-      this.$store.commit("savePassword", this.myPassword);
-      this.$store.commit("saveEmail", this.myEmail);
-      this.data = {
-        name: this.myName,
-        lastName: this.myLastName,
-        password: this.myPassword,
-        email: this.myEmail,
-      };
-      console.log(this.data);
       // Action
       // this.$store.dispatch();
     },
