c6c32f2287b8206caa8ec16e1642221e7f532f95
filipi <filipisee@hotmail.com>
2022-04-07 21:33:32 -0300

    testing getters

--- vuex-complete-guide/src/main.js
+++ vuex-complete-guide/src/main.js
@@ -19,7 +19,18 @@ const store = createStore({
   },
   getters: {
     finalCounter(state) {
+      //console.log(state);
       return state.counter * 3;
+    },
+    normalizedCounter(state, getters) {
+      const finalCounter = getters.finalCounter;
+      if (finalCounter < 0) {
+        return 0;
+      }
+      if (finalCounter > 100) {
+        return 100;
+      }
+      return finalCounter;
     },
   },
 })
