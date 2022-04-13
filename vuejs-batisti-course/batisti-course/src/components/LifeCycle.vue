c8f6e0d4864d96a7d48fd68e218aceb72edb434d
filipi <filipisee@hotmail.com>
2022-04-12 21:06:09 -0300

    tests

---
+++ vuejs-batisti-course/batisti-course/src/components/LifeCycle.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>My name is: {{ myName }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LifeCycle',
+  data() {
+    return {
+      myName: "Don't know yet",
+    }
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
