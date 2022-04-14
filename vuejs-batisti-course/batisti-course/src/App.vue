8938f51db51da7345669ac87873663ffccf987ca
filipi <filipisee@hotmail.com>
2022-04-14 10:42:55 -0300

    tests

--- vuejs-batisti-course/batisti-course/src/App.vue
+++ vuejs-batisti-course/batisti-course/src/App.vue
@@ -22,4 +22,11 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+* {
+  font-family: "Helvetica", sans-serif;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
--- vuejs-batisti-course/batisti-course/src/components/Navbar.vue
+++ vuejs-batisti-course/batisti-course/src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/">
+    <router-link to="/" id="logo-url">
       <img :src="logo" :alt="alt" id="logo">
     </router-link>
     <router-link to="/">Home</router-link> |
@@ -29,5 +29,22 @@ export default {
   display: flex;
   justify-content: flex-end;
   align-items: center;
+}
+#nav #logo-url {
+  margin: auto;
+  margin-left: 0;
+}
+#logo {
+  width: 40px;
+  height: 40px;
+}
+#nav a {
+  color: #fcba03;
+  text-decoration: none;
+  margin: 12px;
+  transition: .5s;
+}
+#nav a:hover {
+  color: #fff;
 }
 </style>
