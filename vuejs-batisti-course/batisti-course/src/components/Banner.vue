69ff2ea4b4551e22c2c235536b02e278a6688fad
filipi <filipisee@hotmail.com>
2022-04-14 21:42:39 -0300

    structuring banner component

---
+++ vuejs-batisti-course/batisti-course/src/components/Banner.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Banner</div>
+</template>
+
+<script>
+export default {
+  name: 'Banner',
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- vuejs-batisti-course/batisti-course/src/views/HomeView.vue
+++ vuejs-batisti-course/batisti-course/src/views/HomeView.vue
@@ -7,5 +7,15 @@
 </template>
 
 <script>
+import Banner from '../components/Banner.vue'
 
+export default {
+  name: 'HomeView',
+  components: {
+    Banner,
+  },
+  data() {
+    return {}
+  },
+}
 </script>
