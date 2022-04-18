76a97b1b54ee3fd54b9b376b152ec6ecc4b4c231
filipi <filipisee@hotmail.com>
2022-04-18 09:19:23 -0300

    added async data to form page

--- vuejs-batisti-course/batisti-course/src/components/LunchForm.vue
+++ vuejs-batisti-course/batisti-course/src/components/LunchForm.vue
@@ -4,39 +4,49 @@
       <form class="lunch-form">
         <div class="input-container">
           <label for="name">Your Name:</label>
-          <input type="text" id="name" name="name" v-model="name" placeholder="Enter your name">
+          <input type="text" 
+                 id="name" 
+                 name="name" 
+                 v-model="name" 
+                 placeholder="Enter your name"/>
         </div>
         <div class="input-container">
           <label for="bread">Choose the bread:</label>
           <select name="bread" id="bread" v-model="bread">
             <option value="">Select your bread</option>
-            <option value="coarse">Coarse bread</option>
+            <option v-for="bread in breadData" 
+                    :key="bread.id"
+                    value="bread-type">
+              {{ bread.type }}
+            </option>
           </select>
         </div>
         <div class="input-container">
           <label for="meat">Choose the meat:</label>
           <select name="meat" id="meat" v-model="meat">
             <option value="">Select your meat</option>
-            <option value="rumptail">Rump tail</option>
+            <option v-for="meat in meatData" 
+                    :key="meat.id"
+                    value="meat-type">
+              {{ meat.type }}
+            </option>
           </select>
         </div>
         <div id="optionals-container" class="input-container">
           <label class="optionals-title" for="meat">Choose the optionals:</label>
           <div class="checkbox-container">
-            <input type="checkbox" name="optionals" v-model="optionals" value="salami">
-            <span>Salami</span>
-          </div>
-          <div class="checkbox-container">
-            <input type="checkbox" name="optionals" v-model="optionals" value="salami">
-            <span>Salami</span>
-          </div>
-          <div class="checkbox-container">
-            <input type="checkbox" name="optionals" v-model="optionals" value="salami">
+            <input type="checkbox" 
+                   name="optionals" 
+                   v-model="optionals" 
+                   value="salami">
             <span>Salami</span>
           </div>
         </div>
         <div class="input-container">
-          <input type="submit" class="submit-btn" value="Create my lunch">
+          <input @click.prevent="showSelectedIngredients" 
+                 type="submit" 
+                 class="submit-btn" 
+                 value="Create my lunch">
         </div>
       </form>
     </div>
@@ -50,10 +60,10 @@ export default {
     return {
       name: undefined,
       bread: undefined,
-      breadData: undefined,
       meat: undefined,
-      meatData: undefined,
       optionals: [],
+      breadData: undefined,
+      meatData: undefined,
       optionalsData: undefined,
       status: "Requested",
       msg: undefined,
@@ -66,7 +76,13 @@ export default {
     async getIngredients() {
       const req = await fetch('http://localhost:3000/ingredients')
       const data = await req.json()
-      console.log(data)
+      
+      this.breadData = data.breads
+      this.meatData = data.meats
+      this.optionalsData = data.optionals
+    },
+    showSelectedIngredients() {
+      console.log(this.name, this.bread, this.meat)
     },
   },
 }
